d236a49fcb1bcdf3550107912f2e0438c2bb3ede
Nathan Tranquilla <nathan.tranquilla@bluespurs.com>
2016-08-10 15:27:27 -0300

    using where instead of length

--- functional/uppercaseLongNames2.js
+++ functional/uppercaseLongNames2.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const { toUpper, map, filter, compose, gt, length, __ } = require('ramda');
+const { toUpper, map, filter, compose, gt, __, where } = require('ramda');
 const gt4=gt(__,4);
 
 const names=["Sally","John","Bart","Joseph"];
 
 const getLongNames= compose(
-    filter(compose(gt4,length)),    //filter in names whose lengths are > 4
-    map(toUpper)                    //each name goes to uppercase
+    filter(where({length: gt(__,4)})),  //filter in names whose lengths are > 4
+    map(toUpper)                        //each name goes to uppercase
 );
 
 console.log(getLongNames(names));
